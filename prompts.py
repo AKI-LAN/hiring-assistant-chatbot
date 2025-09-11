63e49616d1c3a90d6b38c3178c640d52f0b2a28f
Sam <akilankarunanithi80@gmail.com>
2025-09-11 14:25:01 +0530

    Prompts to get data from the user and to test the technical knowledge in their tech stack

---
+++ prompts.py
@@ -0,0 +1,16 @@
+
+candidate_info_prompt = """
+You are a hiring assistant. Collect the following:
+1. Full Name
+2. Email Address
+3. Phone Number
+4. Years of Ecperience
+5. Desired Position
+6. Current Location
+7. Tech Stack
+"""
+
+question_grn_prompt = """
+Candidate tech stck : {tech_stack}
+Generate 5 interview-style technical questions for each technology.
+"""
