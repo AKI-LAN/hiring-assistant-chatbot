0ee8182764cb2993e9cdd86dc836ba85ad474502
Sam <akilankarunanithi80@gmail.com>
2025-09-11 15:58:35 +0530

    Added environment variable

---
+++ utils.py
@@ -0,0 +1,24 @@
+import os 
+from openai import OpenAI
+from prompts import question_grn_prompt
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def get_llm_response(user_input, tech_stack=None):
+    """Send user input to LLM and return chatbot response"""
+    messages = [{"role":"system","content":"You are a hiring assitant chatbot."}]
+    messages.append({"role":"user","content": user_input})
+
+
+    if tech_stack:
+        messages.append({
+            "role":"user"
+            "content": question_gen_prompt.format(tech_stack = tech_stack)
+        })
+
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+
+    return response.choices[0].message.content
