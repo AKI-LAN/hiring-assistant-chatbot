3453e365624d478afe271c13739c2d99ca3394a1
Sam <akilankarunanithi80@gmail.com>
2025-09-12 10:45:24 +0530

    Utils.py is updated to make intiation in the chat with greetings

--- utils.py
+++ utils.py
@@ -5,14 +5,16 @@ generator = pipeline('text2text-generation', model='google/flan-t5-base')
 
 def get_llm_response(user_input, tech_stack=None):
     """
-    Generate a chatbot-like response using Hugging Face DistilGPT-2 model.
+    Generate a chatbot response using Hugging Face Flan-T5 model.
     If tech_stack is provided, include it in the prompt for generating technical questions.
     """
-    prompt = user_input
+    base_instructions = "You are TalentScout, a hiring assistant. Respond politely, ask questions step by step, and gather candidate information.keep answer short"
 
     if tech_stack:
-        prompt += "\n" + question_gen_prompt.format(tech_stack=tech_stack)
+        prompt =  question_gen_prompt.format(tech_stack=tech_stack)
+    else:
+        prompt = f"{base_instructions}\nCandidate said: {user_input}\nYour response:"
 
-    response = generator(prompt, max_length=150, num_return_sequences=1)
+    response = generator(prompt, max_length=200, num_return_sequences=1)
 
     return response[0]['generated_text']
