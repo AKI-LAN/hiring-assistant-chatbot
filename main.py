4a20fe3251fc1a2e2c82a54ec042ffb80722335f
Sam <akilankarunanithi80@gmail.com>
2025-09-12 10:15:20 +0530

    Deleted unwanted files and updated main.py file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import streamlit as st
 from utils import get_llm_response
-from prompts import candidate_info_prompt, question_gen_prompt
-
+from prompts import candidate_info_prompt
 st.set_page_config(page_title="TalentScout Hiring Assistant", layout="centered")
-st.title("TalentScout Hiring Assistant")
+st.title("🤖 TalentScout Hiring Assistant")
 
 if "chat" not in st.session_state:
     st.session_state.chat = []
-if "candidate" not in st.session_state:
-    st.session_state.candidate = {}
 
-user_input = st.text_input("You: ", key="user_input")
+
+user_input = st.text_input("You: ")
 
 if st.button("Send") and user_input:
 
